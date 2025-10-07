5e42394e5f4b4bfe9f8bd66e9084811ce081ecef
martintops <martin.spoto98@gmail.com>
2025-10-07 10:49:27 +0200

    Add PorousPermeability support

--- src/pykarstnsim_demo/models/shared.py
+++ src/pykarstnsim_demo/models/shared.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 class Permeability(enum.Enum):
     Karstified = "Karstified"
     NonKarstified = "NonKarstified"
+    PorousPermeability = "PorousPermeability"
     Undefined = "Undefined"
 
 
 PERMEABILITY_MAP: dict[Permeability, float] = {
     Permeability.Karstified: 0.5,
     Permeability.NonKarstified: 0.0,
+    Permeability.PorousPermeability: 0.0,
     Permeability.Undefined: 0.0,
 }
 
